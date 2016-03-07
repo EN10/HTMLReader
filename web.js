ad1e13cb80b6a5c97385e21730f939cbe0aaa374
Enio <sfmneo@gmail.com>
2016-03-07 20:12:18 +0000

    tweak code

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var request = require('request'); // https://github.com/request/request
 var cheerio = require('cheerio'); // https://github.com/cheeriojs/cheerio
 
 var app = express();
-app.get('/', function(req, res){
-    request('https://news.google.co.uk', function (error, response, body){
+app.get('/', function(req, res) {
+    request('https://news.google.co.uk', function (error, response, body) {
         var $ = cheerio.load(body);
         var html = "";
-        for (var i = 0; i < $('.titletext').get().length; i++)   {
+        for (var i=0; i<$('.titletext').get().length; i++) {
             html += $('.titletext').eq(i).text()+'<p>'; }
         res.send(html);
     });
