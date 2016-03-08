e4d2914d6af52f54f92d7c62edd8090257aeca13
Enio <sfmneo@gmail.com>
2016-03-08 08:19:35 +0000

    add options

---
+++ options.js
@@ -0,0 +1,8 @@
+var options = {
+ url: 'https://news.google.co.uk',
+ headers: {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) "
+ + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.116 Safari/537.36"}
+};
+
+
+// request(options, function (error, response, body) {
